6f0273b9f0da50a0ae88b65aef5c5c88159f4578
Elchanan <elchananm10@gmail.com>
2022-11-20 12:16:46 +0200

    add validations

--- src/main/java/chatApp/service/AuthService.java
+++ src/main/java/chatApp/service/AuthService.java
@@ -8,48 +8,6 @@ import java.util.Map;
 import java.util.Optional;
 
 public class AuthService {
-//    @Autowired
-//    private UserRepository userRepository;
-//    private Map<String, String> usersTokens;
-//    private static final int TOKEN_LENGTH = 10;
 
-//    public String login(String email, String password) {
-//        String token = isValidCredentials(email, password) ? generateToken() : null;
-//
-//        if (token != null) {
-//            this.usersTokens.put(email, token);
-//        }
-//
-//        return token;
-//    }
-
-//    private boolean isValidCredentials(String email, String password) {
-////        Optional<User> user = userRepository.getUserByEmail(email);
-////
-////        return user.filter(value -> value.getPassword().compareTo(password) == 0).isPresent();
-////
-////    }
-//    private static String generateToken() {
-//        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-//        StringBuilder sb = new StringBuilder(TOKEN_LENGTH);
-//
-//        for(int i = 0; i < TOKEN_LENGTH; ++i) {
-//            int index = (int)((double)AlphaNumericString.length() * Math.random());
-//            sb.append(AlphaNumericString.charAt(index));
-//        }
-//
-//        return sb.toString();
-//    }
-//
-//    public boolean register(User user) {
-//        if (isExistingEmail(user.getEmail())) {
-//            throw new IllegalArgumentException("This email address already exists!");
-//        }
-//
-//       // User user = User.createUser(email, name, password);
-//        this.userRepository.addUser(user);
-//
-//        return true;
-//    }
 
 }
